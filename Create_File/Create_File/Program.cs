ace7e1866ac8372f306aa9e6eda498f77e50254a
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-08-24 12:14:38 +0200

    C# Code for RandomTal

---
+++ Create_File/Create_File/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Create_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Create Text File";
+            StartController();
+        }
+
+        #region Controller
+        static void StartController()
+        {
+            Random randon = new Random();
+            int tal;
+            string path = @"C:\RandomTal.txt";
+            using StreamWriter sw = File.CreateText(path);
+            for (int i = 0; i < 1000000; i++)
+            {
+                tal = randon.Next(0, 10000);
+                sw.WriteLine(i.ToString()+ "," + tal);
+            }
+        }
+        #endregion Controller
+
+    }
+}
